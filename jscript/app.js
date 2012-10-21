2392746690979ded024f41d78172c0dbb5d06897
William Wood <william.wood@vineyardcolumbus.org>
2012-10-21 03:31:50 -0400

    adding "less" into my code requirements

--- jscript/app.js
+++ jscript/app.js
@@ -1,4 +1,6 @@
-require(['jquery-1.8.2.min'],function($ww){
+require(['jquery-1.8.2.min','less-1.3.1.min'],function($ww){
 	$ww = jQuery.noConflict();
 	console.log('$ww has been loaded');
+	console.log('less file has been loaded');
+	
 });
