389a1f9ac9fc6585ed1efb0eddec9b5990990c82
William Wood <william.wood@vineyardcolumbus.org>
2012-10-21 03:41:56 -0400

    adding less object to reference

--- jscript/app.js
+++ jscript/app.js
@@ -1,6 +1,8 @@
-require(['jquery-1.8.2.min','less-1.3.1.min'],function($ww){
+require(['jquery-1.8.2.min','less-1.3.1.min'],function($ww,less){
 	$ww = jQuery.noConflict();
 	console.log('$ww has been loaded');
 	console.log('less file has been loaded');
-	
+	$ww('h1').click(function(){
+		console.log('h1 elment was clicked');
+	});
 });
